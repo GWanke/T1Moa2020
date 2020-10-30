40ca7d0b88b2e4dfeacb73f4b30cac2e54ab8269
Gwanke <ra91671@uem.br>
2020-10-30 16:25:11 -0300

    h3 com pd - kira god

--- teste.py
+++ teste.py
@@ -2,7 +2,6 @@
 import heapq
 import time
 tabuleiroFinal=(1,2,3,4,12,13,14,5,11,0,15,6,10,9,8,7)
-import sys
 
 dict_pos = {
     1: (0, 0),
@@ -22,6 +21,249 @@ dict_pos = {
     8: (3, 2),
     7: (3, 3),
 } 
+
+h3memo = [[0 for x in range(16)] for y in range(16)] 
+
+h3memo[1][0] = 0
+h3memo[1][1] = 1
+h3memo[1][2] = 2
+h3memo[1][3] = 3
+h3memo[1][4] = 1
+h3memo[1][5] = 2
+h3memo[1][6] = 3
+h3memo[1][7] = 4
+h3memo[1][8] = 2
+h3memo[1][9] = 3
+h3memo[1][10] = 4
+h3memo[1][11] = 5
+h3memo[1][12] = 3
+h3memo[1][13] = 4
+h3memo[1][14] = 5
+h3memo[1][15] = 6
+h3memo[2][0] = 1
+h3memo[2][1] = 0
+h3memo[2][2] = 1
+h3memo[2][3] = 2
+h3memo[2][4] = 2
+h3memo[2][5] = 1
+h3memo[2][6] = 2
+h3memo[2][7] = 3
+h3memo[2][8] = 3
+h3memo[2][9] = 2
+h3memo[2][10] = 3
+h3memo[2][11] = 4
+h3memo[2][12] = 4
+h3memo[2][13] = 3
+h3memo[2][14] = 4
+h3memo[2][15] = 5
+h3memo[3][0] = 2
+h3memo[3][1] = 1
+h3memo[3][2] = 0
+h3memo[3][3] = 1
+h3memo[3][4] = 3
+h3memo[3][5] = 2
+h3memo[3][6] = 1
+h3memo[3][7] = 2
+h3memo[3][8] = 4
+h3memo[3][9] = 3
+h3memo[3][10] = 2
+h3memo[3][11] = 3
+h3memo[3][12] = 5
+h3memo[3][13] = 4
+h3memo[3][14] = 3
+h3memo[3][15] = 4
+h3memo[4][0] = 3
+h3memo[4][1] = 2
+h3memo[4][2] = 1
+h3memo[4][3] = 0
+h3memo[4][4] = 4
+h3memo[4][5] = 3
+h3memo[4][6] = 2
+h3memo[4][7] = 1
+h3memo[4][8] = 5
+h3memo[4][9] = 4
+h3memo[4][10] = 3
+h3memo[4][11] = 2
+h3memo[4][12] = 6
+h3memo[4][13] = 5
+h3memo[4][14] = 4
+h3memo[4][15] = 3
+h3memo[5][0] = 4
+h3memo[5][1] = 3
+h3memo[5][2] = 2
+h3memo[5][3] = 1
+h3memo[5][4] = 3
+h3memo[5][5] = 2
+h3memo[5][6] = 1
+h3memo[5][7] = 0
+h3memo[5][8] = 4
+h3memo[5][9] = 3
+h3memo[5][10] = 2
+h3memo[5][11] = 1
+h3memo[5][12] = 5
+h3memo[5][13] = 4
+h3memo[5][14] = 3
+h3memo[5][15] = 2
+h3memo[6][0] = 5
+h3memo[6][1] = 4
+h3memo[6][2] = 3
+h3memo[6][3] = 2
+h3memo[6][4] = 4
+h3memo[6][5] = 3
+h3memo[6][6] = 2
+h3memo[6][7] = 1
+h3memo[6][8] = 3
+h3memo[6][9] = 2
+h3memo[6][10] = 1
+h3memo[6][11] = 0
+h3memo[6][12] = 4
+h3memo[6][13] = 3
+h3memo[6][14] = 2
+h3memo[6][15] = 1
+h3memo[7][0] = 6
+h3memo[7][1] = 5
+h3memo[7][2] = 4
+h3memo[7][3] = 3
+h3memo[7][4] = 5
+h3memo[7][5] = 4
+h3memo[7][6] = 3
+h3memo[7][7] = 2
+h3memo[7][8] = 4
+h3memo[7][9] = 3
+h3memo[7][10] = 2
+h3memo[7][11] = 1
+h3memo[7][12] = 3
+h3memo[7][13] = 2
+h3memo[7][14] = 1
+h3memo[7][15] = 0
+h3memo[8][0] = 5
+h3memo[8][1] = 4
+h3memo[8][2] = 3
+h3memo[8][3] = 4
+h3memo[8][4] = 4
+h3memo[8][5] = 3
+h3memo[8][6] = 2
+h3memo[8][7] = 3
+h3memo[8][8] = 3
+h3memo[8][9] = 2
+h3memo[8][10] = 1
+h3memo[8][11] = 2
+h3memo[8][12] = 2
+h3memo[8][13] = 1
+h3memo[8][14] = 0
+h3memo[8][15] = 1
+h3memo[9][0] = 4
+h3memo[9][1] = 3
+h3memo[9][2] = 4
+h3memo[9][3] = 5
+h3memo[9][4] = 3
+h3memo[9][5] = 2
+h3memo[9][6] = 3
+h3memo[9][7] = 4
+h3memo[9][8] = 2
+h3memo[9][9] = 1
+h3memo[9][10] = 2
+h3memo[9][11] = 3
+h3memo[9][12] = 1
+h3memo[9][13] = 0
+h3memo[9][14] = 1
+h3memo[9][15] = 2
+h3memo[10][0] = 3
+h3memo[10][1] = 4
+h3memo[10][2] = 5
+h3memo[10][3] = 6
+h3memo[10][4] = 2
+h3memo[10][5] = 3
+h3memo[10][6] = 4
+h3memo[10][7] = 5
+h3memo[10][8] = 1
+h3memo[10][9] = 2
+h3memo[10][10] = 3
+h3memo[10][11] = 4
+h3memo[10][12] = 0
+h3memo[10][13] = 1
+h3memo[10][14] = 2
+h3memo[10][15] = 3
+h3memo[11][0] = 2
+h3memo[11][1] = 3
+h3memo[11][2] = 4
+h3memo[11][3] = 5
+h3memo[11][4] = 1
+h3memo[11][5] = 2
+h3memo[11][6] = 3
+h3memo[11][7] = 4
+h3memo[11][8] = 0
+h3memo[11][9] = 1
+h3memo[11][10] = 2
+h3memo[11][11] = 3
+h3memo[11][12] = 1
+h3memo[11][13] = 2
+h3memo[11][14] = 3
+h3memo[11][15] = 4
+h3memo[12][0] = 1
+h3memo[12][1] = 2
+h3memo[12][2] = 3
+h3memo[12][3] = 4
+h3memo[12][4] = 0
+h3memo[12][5] = 1
+h3memo[12][6] = 2
+h3memo[12][7] = 3
+h3memo[12][8] = 1
+h3memo[12][9] = 2
+h3memo[12][10] = 3
+h3memo[12][11] = 4
+h3memo[12][12] = 2
+h3memo[12][13] = 3
+h3memo[12][14] = 4
+h3memo[12][15] = 5
+h3memo[13][0] = 2
+h3memo[13][1] = 1
+h3memo[13][2] = 2
+h3memo[13][3] = 3
+h3memo[13][4] = 1
+h3memo[13][5] = 0
+h3memo[13][6] = 1
+h3memo[13][7] = 2
+h3memo[13][8] = 2
+h3memo[13][9] = 1
+h3memo[13][10] = 2
+h3memo[13][11] = 3
+h3memo[13][12] = 3
+h3memo[13][13] = 2
+h3memo[13][14] = 3
+h3memo[13][15] = 4
+h3memo[14][0] = 3
+h3memo[14][1] = 2
+h3memo[14][2] = 1
+h3memo[14][3] = 2
+h3memo[14][4] = 2
+h3memo[14][5] = 1
+h3memo[14][6] = 0
+h3memo[14][7] = 1
+h3memo[14][8] = 3
+h3memo[14][9] = 2
+h3memo[14][10] = 1
+h3memo[14][11] = 2
+h3memo[14][12] = 4
+h3memo[14][13] = 3
+h3memo[14][14] = 2
+h3memo[14][15] = 3
+h3memo[15][0] = 4
+h3memo[15][1] = 3
+h3memo[15][2] = 2
+h3memo[15][3] = 3
+h3memo[15][4] = 3
+h3memo[15][5] = 2
+h3memo[15][6] = 1
+h3memo[15][7] = 2
+h3memo[15][8] = 2
+h3memo[15][9] = 1
+h3memo[15][10] = 0
+h3memo[15][11] = 1
+h3memo[15][12] = 3
+h3memo[15][13] = 2
+h3memo[15][14] = 1
+h3memo[15][15] = 2
     
 class Peca():
     def __init__(self, tabuleiro, indexZero, g = 0):
@@ -54,8 +296,8 @@ def Spiral(matriz):
     #print (type(matriz))
     #RECURSIVIDADE. -> Tira a primeira linha da matriz, rotaciona a matriz, adiciona a primeira linha.
     #Ex: [0 1 2 3] , [4,5,6,7] , [8,9,10,11].[12,13,14,15] = [0,1,2,3] + Spiral[[7,11,15],[6,10,14],[5,9,13],[4,8,12]] = ...
-    #return matriz and list(matriz.pop(0)) + Spiral(zip(*matriz)[::-1])      #->Python 2
-    return matriz and [*matriz.pop(0)] + Spiral([*zip(*matriz)][::-1])     #->Python 3
+    return matriz and list(matriz.pop(0)) + Spiral(zip(*matriz)[::-1])      #->Python 2
+    #return matriz and [*matriz.pop(0)] + Spiral([*zip(*matriz)][::-1])     #->Python 3
 
 def heuristicaDois(tabuleiro):
     caracol=list(range(0,16))
@@ -79,13 +321,19 @@ def heuristicaDois(tabuleiro):
 
 
 
+# def heuristicaTres(tabuleiro):
+#     ManhattamDist = 0
+#     for i,item in enumerate(tabuleiro):
+#         xAtual,yAtual = int(i/ 4) , i % 4
+#         xObjetivo,yObjetivo = dict_pos[item][0],dict_pos[item][1]
+#         ManhattamDist += abs(xAtual - xObjetivo) + abs(yAtual - yObjetivo)
+#     return ManhattamDist
 def heuristicaTres(tabuleiro):
-    ManhattamDist = 0
-    for i,item in enumerate(tabuleiro):
-        xAtual,yAtual = int(i/ 4) , i % 4
-        xObjetivo,yObjetivo = dict_pos[item][0],dict_pos[item][1]
-        ManhattamDist += abs(xAtual - xObjetivo) + abs(yAtual - yObjetivo)
-    return ManhattamDist
+    resp = 0
+    for i in range(16):
+        if (tabuleiro[i] != 0):
+            resp += h3memo[tabuleiro[i]][i] 
+    return resp
 
 def heuristicaQuatro(tabuleiro):
     h1=heuristicaUm(tabuleiro)
@@ -107,11 +355,6 @@ def swap_tuple(set_to_swap, index_origin, index_dest):
     to_list = list(set_to_swap)
     to_list[index_dest], to_list[index_origin] = to_list[index_origin], to_list[index_dest]
     return tuple(to_list)
-def delete_tuple(set_to_delete):
-    to_list=list(set_to_delete)
-    to_list.pop(0)
-    print(to_list)
-    return tuple(to_list)
 
 def geraSucessores(noPai):
     if noPai.indexZero >= 4:
@@ -131,14 +374,13 @@ def geraSucessores(noPai):
         yield swap_tuple(noPai.tabuleiro, noPai.indexZero, newIndexNulo), newIndexNulo
 
 def AEstrela(noI):
-    start = time.process_time()
+    #start = time.process_time()
     listaAberta = []        #heapq 
     listaFechada = set()   #set
     auxOpen={}
     heapq.heappush(listaAberta, noI)
     selecionado = heapq.heappop(listaAberta)
-    count=0
-    
+
     while selecionado.tabuleiro != tabuleiroFinal:
         listaFechada.add(selecionado.tabuleiro)
         
@@ -155,8 +397,8 @@ def AEstrela(noI):
                 auxOpen[hash(new_tabuleiro)] = filho
         
         selecionado = heapq.heappop(listaAberta) 
-    print(len(listaAberta))
-    print(time.process_time() - start)
+    #print(len(listaAberta),len(listaFechada),len(auxOpen))
+    #print(time.process_time() - start)
     return selecionado.g
 
 
